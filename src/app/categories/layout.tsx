8744ec2573fcf37d024834864773a82b01198cac
Daniel Schmitz <danieljfa@gmail.com>
2024-03-21 10:47:30 -0300

    Category start screens

---
+++ src/app/categories/layout.tsx
@@ -0,0 +1,10 @@
+import { PropsWithChildren } from "react";
+
+export default function layout({ children }: PropsWithChildren) {
+  return (
+    <div>
+      <h2>Categories</h2>
+      <div>{children}</div>
+    </div>
+  );
+}

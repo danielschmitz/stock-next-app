200e0fcfdee2437cfe2be8543247181b6a0b7eab
Daniel Schmitz <danieljfa@gmail.com>
2024-03-30 20:26:40 -0300

    Styling with @apply

--- src/app/categories/new/page.tsx
+++ src/app/categories/new/page.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 export default function page() {
   return (
     <div>
-      <h2 className="text-cyan-950 text-md font-semibold">New Category</h2>
+      <h2>New Category</h2>
       <div className="flex flex-row w-full m-5">form</div>
       <Button asChild variant="outline">
         <Link href="/categories">Back</Link>

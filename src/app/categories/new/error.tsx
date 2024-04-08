52986a6b78df1a658d8c1fbba4ed5f56b3e6e897
Daniel Schmitz <danieljfa@gmail.com>
2024-04-08 19:53:56 -0300

    Error Message

---
+++ src/app/categories/new/error.tsx
@@ -0,0 +1,27 @@
+'use client'
+
+import { Button } from '@/components/ui/button'
+
+export default function Error({
+  error,
+  reset
+}: {
+  error: Error & { digest?: string }
+  reset: () => void
+}) {
+  return (
+    <div>
+      <h2>Something went wrong!</h2>
+      <b>{error.message}</b>
+      <br />
+      <Button
+        onClick={
+          // Attempt to recover by trying to re-render the segment
+          () => reset()
+        }
+      >
+        Try again
+      </Button>
+    </div>
+  )
+}

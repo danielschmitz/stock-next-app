f53761f78a0e7413788db0ab79e6340fe479498e
Daniel Schmitz <danieljfa@gmail.com>
2024-04-10 20:13:10 -0300

    List all categories

--- src/app/categories/page.tsx
+++ src/app/categories/page.tsx
@@ -1,9 +1,16 @@
 import { Button } from '@/components/ui/button'
+import { PrismaClient } from '@prisma/client'
 import Link from 'next/link'
 
-export default function page() {
+export default async function page() {
+  const prisma = new PrismaClient()
+  const categories = await prisma.category.findMany()
+
   return (
     <div>
+      {categories.map((category) => (
+        <div key={category.id}>{category.name}</div>
+      ))}
       <Button asChild>
         <Link href="/categories/new">New</Link>
       </Button>

0f2aa3207627e570de0774004fe73f7ecc66a277
Daniel Schmitz <danieljfa@gmail.com>
2024-04-10 20:22:56 -0300

    Using Table Component to list  categories

--- src/app/categories/page.tsx
+++ src/app/categories/page.tsx
@@ -1,4 +1,13 @@
 import { Button } from '@/components/ui/button'
+import {
+  Table,
+  TableCaption,
+  TableHeader,
+  TableRow,
+  TableHead,
+  TableBody,
+  TableCell
+} from '@/components/ui/table'
 import { PrismaClient } from '@prisma/client'
 import Link from 'next/link'
 
@@ -8,11 +17,35 @@ export default async function page() {
 
   return (
     <div>
-      {categories.map((category) => (
-        <div key={category.id}>{category.name}</div>
-      ))}
+      <Table>
+        <TableCaption>A list of your categories.</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Name</TableHead>
+            <TableHead className="w-[100px] text-center">Actions</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {categories.map((category) => (
+            <TableRow key={category.id}>
+              <TableCell>{category.name}</TableCell>
+              <TableCell className="text-right">
+                <div className="flex flex-row gap-2">
+                  <Button asChild variant="link">
+                    <Link href={`/categories/edit/${category.id}`}>Edit</Link>
+                  </Button>
+                  <Button asChild variant="link">
+                    <Link href={`/categories/del/${category.id}`}>Delete</Link>
+                  </Button>
+                </div>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+
       <Button asChild>
-        <Link href="/categories/new">New</Link>
+        <Link href="/categories/new">New Category</Link>
       </Button>
     </div>
   )
